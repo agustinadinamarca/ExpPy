10ffd5d012cc310454efcc3223356eadc03296f1
Agustina Dinamarca <agustinadinamarca@gmail.com>
2021-03-06 15:17:38 -0300

    Add files via upload

---
+++ run_experiments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file="configurations.csv"
+
+if [[ -f "$file" ]]
+then
+    i=1 n=0
+    while read -r line
+        do
+            ((n >= i )) && [[ ${line:0:1} == "0" ]] && python3 module/main.py "$line"
+            ((n++))
+    done < "$file"
+else
+    echo "$file not found!"  
+fi
