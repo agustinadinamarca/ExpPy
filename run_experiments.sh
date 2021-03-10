66d68a097f708651e1e04f445613c3cd68531c44
Agustina Dinamarca <agustinadinamarca@gmail.com>
2021-03-10 18:33:44 -0300

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -7,7 +7,7 @@ then
     i=1 n=0
     while read -r line
         do
-            ((n >= i )) && [[ ${line:0:1} == "0" ]] && python3 module/main.py "$line"
+            ((n >= i )) && [[ ${line:0:1} == "0" ]] || [[ ${line:0:1} == "i" ]] && python3 module/main.py "$line"
             ((n++))
     done < "$file"
 else
